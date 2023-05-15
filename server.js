4ddcbcdbd82a2459150c6a81da010be2ec0e73bf
amiogus <130595965+amiogus@users.noreply.github.com>
2023-05-15 11:08:33 -0700

    Create server.js
    server

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/rplace', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const pixelSchema = new mongoose.Schema({
+  x: Number,
+  y: Number,
+  color: String,
+});
+
+const Pixel = mongoose.model('Pixel', pixelSchema);
+
+app.get('/pixels', async (req, res) => {
+  const pixels = await Pixel.find();
+  res.send(pixels);
+});
+
+app.post('/place', async (req, res) => {
+  const { x, y, color } = req.body;
+  const existingPixel = await Pixel.findOne({ x, y });
+  if (existingPixel) {
+    existingPixel.color = color;
+    await existingPixel.save();
+  } else {
+    const pixel = new Pixel({ x, y, color });
+    await pixel.save();
+  }
+  res.send({ success: true });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
